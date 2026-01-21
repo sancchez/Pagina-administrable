3734844347fcaa85cd63eb7e5d7d6d03388c8486
Usuario <usuario@example.com>
2026-01-20 21:40:33 -0500

    cambio deply

--- deploy.sh
+++ deploy.sh
@@ -226,4 +226,4 @@ pm2 logs pagina-admin --lines 20 --nostream
 
 echo ""
 log_success "🎉 Todo listo! El servidor está corriendo."
-echo "Weimar s cambio de deploy.sh"
+echo "Weimar s y camilo cambio de deploy.sh"
